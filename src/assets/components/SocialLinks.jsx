df07219f4ac6cf077cab37f3805918e19694c9da
Riya Kaushik Dharsandiya <riyadharsandiya@Riyas-MacBook-Air.local>
2024-05-03 21:10:49 +0530

    initial commit

--- src/assets/components/SocialLinks.jsx
+++ src/assets/components/SocialLinks.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import {FaGithub,FaLinkedin}from 'react-icons/fa'
 import {HiOutlineMail}from 'react-icons/hi'
-import {BsFillPersonLinesFill}from 'react-icons/bs'
+
+
 const SocialLinks = () => {
     const links=[
         {
@@ -32,18 +33,6 @@ const SocialLinks = () => {
             ),
             href:"mailto:riyadharsandiya17@gmail.com",
         },
-        {
-            id:4,
-            child:(
-                <>
-                Resume <BsFillPersonLinesFill size={30}/>
-                </>
-            ),
-            href:"/public/Riya-Dharsandiya-resume.pdf",
-            style:'rounded-br-md',
-            download:true,
-            
-        }
     ]
   return (
     <div className='hidden lg:flex flex-col top-[35%] left-0 fixed'>
@@ -52,7 +41,6 @@ const SocialLinks = () => {
             <li key={id} className={`flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:rounded-md duration-300 hover:ml-[-10px] ${style}`}>
             <a href={href} 
             className='flex justify-between items-center w-full text-white'
-            download={download}
             target='_blank'
             rel="noreferrer"
             >
